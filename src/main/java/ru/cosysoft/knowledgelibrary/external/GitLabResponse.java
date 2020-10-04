1ef0c557c70218385fbaddaf37b7f716f716ed00
Vladislav Chunikhin <vlad.chunihin@cosysoft.ru>
2020-10-04 14:09:49 +0400

    search by keyword

---
+++ src/main/java/ru/cosysoft/knowledgelibrary/external/GitLabResponse.java
@@ -0,0 +1,6 @@
+package ru.cosysoft.knowledgelibrary.external;
+
+import java.io.Serializable;
+
+public interface GitLabResponse extends Serializable {
+}
--- src/main/java/ru/cosysoft/knowledgelibrary/web/KnowledgeLibraryController.java
+++ src/main/java/ru/cosysoft/knowledgelibrary/web/KnowledgeLibraryController.java
@@ -1,18 +1,31 @@
 package ru.cosysoft.knowledgelibrary.web;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import ru.cosysoft.knowledgelibrary.external.GitLabResponse;
+import ru.cosysoft.knowledgelibrary.external.Projects;
 
 @RestController
 @RequestMapping("/knowledge-library")
@@ -32,8 +45,8 @@ public class KnowledgeLibraryController {
         return "Hello from publishKnowledge";
     }
 
-    @GetMapping
-    public ResponseEntity<String> findKnowledge(
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public Collection<Projects> findKnowledge(
         @RequestParam(value = "tags", required = false) final Collection<String> tags,
         @RequestParam(value = "keyword", required = false) final String keyword
     ) {
@@ -41,8 +54,49 @@ public class KnowledgeLibraryController {
         final HttpHeaders headers = new HttpHeaders();
         headers.set("PRIVATE-TOKEN", this.accessToken);
         final HttpEntity<Object> httpEntity = new HttpEntity<>(null, headers);
-        final ResponseEntity<String> projectsListResult =
-            this.restTemplate.exchange(projectList, HttpMethod.GET, httpEntity, String.class);
-        return projectsListResult;
+
+        final ParameterizedTypeReference<Collection<Projects>> parameterizedTypeReference =
+            new ParameterizedTypeReference<Collection<Projects>>() {
+            };
+        final ResponseEntity<Collection<Projects>> projectsListResult =
+            this.restTemplate.exchange(projectList, HttpMethod.GET, httpEntity, parameterizedTypeReference);
+        final Collection<Projects> projects = projectsListResult.getBody();
+
+        if (CollectionUtils.isEmpty(projects)) {
+            return projects;
+        }
+        final List<Projects> projectsWithKnowledgeSharing =
+            projects.stream()
+                .filter(project -> project.getHttp_url_to_repo().contains("knowledge-sharing"))
+                .filter(project -> this.isValidReadme(project, keyword))
+                .collect(Collectors.toList());
+
+        return projectsWithKnowledgeSharing;
+    }
+
+    @SneakyThrows
+    private boolean isValidReadme(final Projects projects, final String keyword) {
+        final String projectList = this.apiUrl + "projects/" + projects.getId() + "/repository/files/README.md?ref=master";
+        final HttpHeaders headers = new HttpHeaders();
+        headers.set("PRIVATE-TOKEN", this.accessToken);
+        final HttpEntity<Object> httpEntity = new HttpEntity<>(null, headers);
+        final ResponseEntity<GitLabFile> projectsListResult =
+            this.restTemplate.exchange(projectList, HttpMethod.GET, httpEntity, GitLabFile.class);
+        final GitLabFile gitLabFile = projectsListResult.getBody();
+        if (gitLabFile == null) {
+            return false;
+        }
+        final byte[] content = Base64.getDecoder().decode(gitLabFile.getContent());
+        final String contentAsStr = new String(content, StandardCharsets.UTF_8);
+        return contentAsStr.contains(keyword);
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    private static class GitLabFile implements GitLabResponse {
+        private String file_name;
+        private String encoding;
+        private String content;
     }
 }
